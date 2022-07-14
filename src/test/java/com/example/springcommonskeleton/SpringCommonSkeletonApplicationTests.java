f2594fdca88674287c33d2287ed56369c9bd4d51
jiwoo.kim <jiwoo.kim@deliveryhero.co.kr>
2022-07-14 16:36:37 +0900

    first commit

---
+++ src/test/java/com/example/springcommonskeleton/SpringCommonSkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcommonskeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCommonSkeletonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
